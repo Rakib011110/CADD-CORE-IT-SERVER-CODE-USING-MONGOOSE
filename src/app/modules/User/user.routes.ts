e887af1502e6128c06feb03cb6993ecfe554f5e6
Rakib011110 <rakib088880@gmail.com>
2025-03-16 10:57:09 +0600

    user delete update getadded

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -1,13 +1,11 @@
 import express from "express";
-import {  UserControllers } from "./user.controller";
+import { UserControllers } from "./user.controller";
 
 const router = express.Router();
 
-
-router.post(
-    '/create-user',
-   
-    UserControllers.createUsers
-  );
-
-export const UserRoutes= router;
+router.post("/create-user", UserControllers.createUsers);
+router.get("/", UserControllers.getAllUsers); 
+router.get("/:id", UserControllers.getAUser); 
+router.put("/:id", UserControllers.updateUser); 
+router.delete("/:id", UserControllers.deleteUser); 
+export const UserRoutes = router;
--- src/app/modules/User/user.services.ts
+++ src/app/modules/User/user.services.ts
@@ -1,18 +1,72 @@
 import { TUser } from "./user.interface";
 import userModel from "./user.model";
 
- 
+const createUserIntoDB = async (payload: TUser) => {
+  try {
+    const newUser = await userModel.create(payload);
+    return newUser;
+  } catch (error) {
+    throw error;
+  }
+};
 
- const createUserIntoDB = async (payload: TUser) => {
-    try {
-      const newUser = await userModel.create(payload);
-      return newUser;
-    } catch (error) {
-      throw error;
+const getAllUsersFromDb = async () => {
+  try {
+    const users = await userModel.find();
+    return users;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const getAUserFromDb = async (id: string) => {
+  try {
+    const user = await userModel.findById(id);
+    if (!user) {
+      throw new Error("User not found");
     }
-  }  
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
+ 
+const updpateUserInDb = async (id: string, payload: TUser) => {
+  try {  
+const user = await userModel.findByIdAndUpdate(id, payload, { new: true });
+    if (!user) {
+      throw new Error("User not found");
+    } 
+
+    return user;
+  } catch (error) {
+    throw error;
 
 
-  export const UserServices = {
-    createUserIntoDB,       
-  };
+
+   } };
+
+
+const deleteUserFromDb = async (id: string) => {
+  try {    
+
+   const deletedUser = await userModel.findByIdAndDelete(id);
+
+
+   if (!deletedUser) {
+    throw new Error("User not found");
+  }
+
+  return deletedUser;
+   } catch (error) {} };
+
+
+
+export const UserServices = {
+  createUserIntoDB,
+  getAllUsersFromDb,
+  getAUserFromDb, 
+  updpateUserInDb, 
+
+  deleteUserFromDb
+};
